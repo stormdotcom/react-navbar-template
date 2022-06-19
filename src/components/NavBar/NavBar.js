ff39d5c194c909fdf6fb3304330ef2f248e9e88a
Ajmal <ajmaln73@gmail.com>
2022-06-19 11:03:52 +0530

    finals

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import classes from './NavBar.module.css'
+import './link.css'
 const NavBar = () => {
+  const [toggleNav, setToggleNav] = useState(false);
+  const handleNavLink = ()=> setToggleNav(!toggleNav);
+
   return (
     <div className={classes.nav}>
         <div className={classes.logo}>
           <i className='fas fa-mountain'></i>
           <h4>Mountains</h4>
         </div>
-        <div className={classes.navlinks}>
-           <div className={classes.link}> <h5>Home </h5> </div>  
-           <div className={classes.link}> <h5> About</h5> </div>  
-           <div className={classes.link}> <h5> Service </h5> </div>  
-           <div className={classes.link}> <h5>Contact Us </h5> </div>  
+        <div className={toggleNav ? 'navlinks active': 'navlinks'}>
+           <div className='link'> <h5>Home </h5> </div>  
+           <div className='link'> <h5> About</h5> </div>  
+           <div className='link'> <h5> Service </h5> </div>  
+           <div className='link'> <h5>Contact Us </h5> </div>  
+        </div>
+        <div onClick={handleNavLink} className='hamburgToggle'>
+            <i className='fas fa-bars fa-lg'></i>
         </div>
     </div>
   )
